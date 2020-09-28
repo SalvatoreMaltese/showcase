efa5921b50c2603346557580a6562eb1b689f89d
MalteseInformatik <49616156+SalvatoreMaltese@users.noreply.github.com>
2020-09-28 05:40:58 +0200

    Add files via upload

---
+++ ClientApp/build/precache-manifest.243d3318368d731eecfb3a46011ecbb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef0d0b454807aa84fde3e188c66d0cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6946e000299ac52a603",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "176eee818e54e49baf92",
+    "url": "/static/css/main.d4c76d95.chunk.css"
+  },
+  {
+    "revision": "b6946e000299ac52a603",
+    "url": "/static/js/2.6e9c11b1.chunk.js"
+  },
+  {
+    "revision": "6b3cb6c3969d18d8a1f43bf907f9571f",
+    "url": "/static/js/2.6e9c11b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176eee818e54e49baf92",
+    "url": "/static/js/main.ce2a2ece.chunk.js"
+  },
+  {
+    "revision": "455a30fa6c258efddd75",
+    "url": "/static/js/runtime-main.44a4a023.js"
+  }
+]);
